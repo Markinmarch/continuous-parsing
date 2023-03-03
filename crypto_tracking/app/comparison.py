d34c3e0a4cc3e079dfa4ee8cf42215efeaa3824a
Mark Izotov <iztvmrk@mail.ru>
2023-03-03 03:47:39 +0300

    рефакторинг отображения

--- crypto_tracking/app/comparison.py
+++ crypto_tracking/app/comparison.py
@@ -47,13 +47,13 @@ class CryptoTracking():
             min_price = min(ready_list)
             max_percent = max_price/100
             min_percent = min_price/100
-            if current_percent < min_percent:
+            if current_percent < min_percent + 1:
                 print(f'{now_time} ФЬЮЧЕРС ETHUSDT УПАЛ НА ↓{(min_price - ready_price).__round__(2)}$ '
-                    f'↓{((((min_percent-current_percent)/min_percent)*100)+1).__round__(3)} % '
+                    f'↓{((((min_percent-current_percent)/min_percent)*100)).__round__(3)} % '
                     f'ТЕКУЩИЙ КУРС ↓{ready_price}$')
-            elif current_percent > max_percent:
+            elif current_percent > max_percent + 1:
                 print(f'{now_time} ФЬЮЧЕРС ETHUSDT ВЫРОС НА ↑{(ready_price - max_price).__round__(2)}$ '
-                    f'↑{((((current_percent-max_percent)/max_percent)*100)+1).__round__(3)} % '
+                    f'↑{((((current_percent-max_percent)/max_percent)*100)).__round__(3)} % '
                     f'ТЕКУЩИЙ КУРС ↑{ready_price}$')
         return self.cache.lpush('ready_list', ready_price)
 
